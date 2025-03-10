65f41bd44a0090c0a143f1c829208a01311165ca
MACBOOK <macbook@Maximus-MacBook-Pro.local>
2025-03-10 22:15:14 +0100

    completed tailiwnd setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="justify-center items-center flex-1"
+    >
+      <Text className="text-5xl text-dark-200 font-bold">Welcome</Text>
+    </View>
+  );
+}
